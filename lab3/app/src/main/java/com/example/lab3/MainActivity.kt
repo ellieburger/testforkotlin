d355144662a82f23f0268b4b89b24e1e7d42a48d
ellieburger <lee.liz2002ilovenh1d520@gmail.com>
2023-10-25 02:24:14 +0800

    new one

--- lab3/app/src/main/java/com/example/lab3/MainActivity.kt
+++ lab3/app/src/main/java/com/example/lab3/MainActivity.kt
@@ -11,30 +11,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //將變數與 XML 元件綁定
-        val ed_name = findViewById<EditText>(R.id.ed_name)
-        val tv_text = findViewById<TextView>(R.id.tv_text)
-        val btn_scissor = findViewById<RadioButton>(R.id.btn_scissor)
-        val btn_stone = findViewById<RadioButton>(R.id.btn_stone)
-        val btn_paper = findViewById<RadioButton>(R.id.btn_paper)
-        val btn_mora = findViewById<Button>(R.id.btn_mora)
-        val tv_name = findViewById<TextView>(R.id.tv_name)
-        val tv_winner = findViewById<TextView>(R.id.tv_winner)
-        val tv_mmora = findViewById<TextView>(R.id.tv_mmora)
-        val tv_cmora = findViewById<TextView>(R.id.tv_cmora)
-        btn_mora.setOnClickListener {
+        val input_name = findViewById<EditText>(R.id.input_name)
+        val text_show = findViewById<TextView>(R.id.text_show)
+        val btn_sci = findViewById<RadioButton>(R.id.btn_sci)
+        val btn_rock = findViewById<RadioButton>(R.id.btn_rock)
+        val btn_pa = findViewById<RadioButton>(R.id.btn_pa)
+        val button = findViewById<Button>(R.id.button)
+        val text_name = findViewById<TextView>(R.id.text_name)
+        val text_win = findViewById<TextView>(R.id.text_win)
+        val text_myturn = findViewById<TextView>(R.id.text_myturn)
+        val text_comturn = findViewById<TextView>(R.id.text_comturn)
+        button.setOnClickListener {
             //判斷 EditText 的字數是否小於一，若成立則無法進行猜拳
-            if (ed_name.length() < 1) {
-                tv_text.text = "請輸入玩家姓名"
+            if (input_name.length() < 1) {
+                text_show.text = "請輸入玩家姓名"
                 return@setOnClickListener
             }
             //取出 EditText 文字作為玩家姓名並用變數儲存
-            val playerName = ed_name.text
+            val playerName = input_name.text
             //亂數產生介於 0~1 之間不含 1 的小數，乘 3 變成 0~2 作為電腦的出拳
             val comMora = (Math.random() * 3).toInt()
             //將玩家出拳結果對應成字串並用變數儲存
             val playerMoraText = when {
-                btn_scissor.isChecked -> "剪刀"
-                btn_stone.isChecked -> "石頭"
+                btn_sci.isChecked -> "剪刀"
+                btn_rock.isChecked -> "石頭"
                 else -> "布"
             }
             //將電腦出拳結果對應成字串並用變數儲存
@@ -44,26 +44,26 @@ class MainActivity : AppCompatActivity() {
                 else -> "布"
             }
             //顯示玩家姓名與雙方出拳結果
-            tv_name.text = "名字\n$playerName"
-            tv_mmora.text = "我方出拳\n$playerMoraText"
-            tv_cmora.text = "電腦出拳\n$comMoraText"
+            text_name.text = "名字\n$playerName"
+            text_myturn.text = "我方出拳\n$playerMoraText"
+            text_comturn.text = "電腦出拳\n$comMoraText"
             //用三個判斷式決定勝負並顯示猜拳結果
             when {
-                btn_scissor.isChecked && comMora == 2 ||
-                        btn_stone.isChecked && comMora == 0 ||
-                        btn_paper.isChecked && comMora == 1 -> {
-                    tv_winner.text = "勝利者\n$playerName"
-                    tv_text.text = "恭喜你獲勝了！！！"
+                btn_sci.isChecked && comMora == 2 ||
+                        btn_rock.isChecked && comMora == 0 ||
+                        btn_pa.isChecked && comMora == 1 -> {
+                    text_win.text = "勝利者\n$playerName"
+                    text_show.text = "你贏了！！！"
                 }
-                btn_scissor.isChecked && comMora == 1 ||
-                        btn_stone.isChecked && comMora == 2 ||
-                        btn_paper.isChecked && comMora == 0 -> {
-                    tv_winner.text = "勝利者\n 電腦"
-                    tv_text.text = "可惜，電腦獲勝了！"
+                btn_sci.isChecked && comMora == 1 ||
+                        btn_rock.isChecked && comMora == 2 ||
+                        btn_pa.isChecked && comMora == 0 -> {
+                    text_win.text = "勝利者\n 電腦"
+                    text_show.text = "輸惹......"
                 }
                 else -> {
-                    tv_winner.text = "勝利者\n 平手"
-                    tv_text.text = "平局，請再試一次！"
+                    text_win.text = "勝利者\n 平手"
+                    text_show.text = "平手~再來一局？"
                 }
             }
         }
